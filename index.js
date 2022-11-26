f74c671720b171023ec879819d312181b4170b91
syed-ahammad <Syed_Ahammad@outlook.com>
2022-11-26 19:08:45 +0600

    post api added for users

--- index.js
+++ index.js
@@ -11,16 +11,31 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zvgiq1x.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true,
+serverApi: ServerApiVersion.v1,
 });
 
-console.log(uri);
+// console.log(uri);
 
 async function run() {
   try {
+    const usersCollection = client.db('resaleMarket').collection('users');
+
+    // api for save user in mongodb
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const adminQuery = {role: "admin"}
+      const adminGet = await usersCollection.findOne(adminQuery);
+      if(adminGet == null){
+        user.role = "admin"
+      }
+      const result = await usersCollection.insertOne(user);
+      console.log(user)
+      res.send(user);
+
+  });
+    
+
   } finally {
   }
 }
