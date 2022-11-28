89db3a2d2d0563b7d93f03f211529f8c178775f6
syed-ahammad <Syed_Ahammad@outlook.com>
2022-11-28 15:31:08 +0600

    api added for find category

--- index.js
+++ index.js
@@ -11,69 +11,92 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zvgiq1x.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true,
-serverApi: ServerApiVersion.v1,
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 // console.log(uri);
 
 async function run() {
   try {
-    const usersCollection = client.db('resaleMarket').collection('users');
-    const productsCollection = client.db('resaleMarket').collection('products');
+    const usersCollection = client.db("resaleMarket").collection("users");
+    const productsCollection = client.db("resaleMarket").collection("products");
+    const blogsCollection = client.db("resaleMarket").collection("blogs");
 
     // get api for get all product from database
-    app.get('/products', async(req, res)=>{
+    app.get("/products", async (req, res) => {
       const query = {};
       const allproducts = await productsCollection.find(query).toArray();
       req.res(allproducts);
     });
 
     // api for specific user product finding
-    app.get('/seller/myproduct', async (req, res)=>{
+    app.get("/seller/myproduct", async (req, res) => {
       const email = req.query.email;
       // console.log(email)
-      const query = {email: email};
+      const query = { email: email };
       const products = await productsCollection.find(query).toArray();
-      console.log(products)
-  
+      console.log(products);
 
       res.send(products);
-  });
+    });
 
     //  get api create for testing admin
-    app.get('/user/admin/:email', async(req,res)=>{
+    app.get("/user/admin/:email", async (req, res) => {
       const email = req.params.email;
-      console.log(email)
-      const query = {email: email}
+      console.log(email);
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      res.send({isAdmin: user?.role === 'admin'});
-  });
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    // api for get product by category
+    app.get("/category", async (rep, res) => {
+      const allCategory = [];
+      const query = {};
+      const products = await productsCollection.find(query).toArray();
+      const categories = products.forEach((product) => {
+        const findCategory = product.category;
+        const getCategory = allCategory.find(
+          (category) => category == findCategory
+        );
+        if (!getCategory) {
+          allCategory.push(findCategory);
+        }
+      });
+      // console.log(allCategory);
+      res.send(allCategory);
+    });
+
+    // api for get blogs post from database
+    app.get("/blogs", async (req, res) => {
+      const query = {};
+      const blogs = await blogsCollection.find(query).toArray();
+      res.send(blogs);
+    });
 
     // api for save user in mongodb
-    app.post('/users', async(req, res)=>{
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const adminQuery = {role: "admin"}
+      const adminQuery = { role: "admin" };
       const adminGet = await usersCollection.findOne(adminQuery);
-      if(adminGet == null){
-        user.role = "admin"
+      if (adminGet == null) {
+        user.role = "admin";
       }
       const result = await usersCollection.insertOne(user);
-      console.log(user)
+      console.log(user);
       res.send(user);
-
-  });
+    });
 
     // api for save product in mongodb
-    app.post('/addproduct', async(req, res)=>{
+    app.post("/addproduct", async (req, res) => {
       const product = req.body;
       const result = await productsCollection.insertOne(product);
-      console.log(product)
+      console.log(product);
       res.send(result);
-
-  });
-    
-
+    });
   } finally {
   }
 }
