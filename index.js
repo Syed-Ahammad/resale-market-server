8b0c41b444eb445bb24dbabed943aa31a8908a26
syed-ahammad <Syed_Ahammad@outlook.com>
2022-11-30 14:09:48 +0600

    seller testing api added

--- index.js
+++ index.js
@@ -71,6 +71,14 @@ async function run() {
       const user = await usersCollection.findOne(query);
       res.send({ isBuyer: user?.role === "buyer" });
     });
+    //  api create for testing seller
+    app.get("/user/seller/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isBuyer: user?.role === "seller" });
+    });
 
     // api for get product by category
     app.get("/category", async (rep, res) => {
